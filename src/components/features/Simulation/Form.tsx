c4d796591583f77b609e2a1ef06558ce4ffd7466
GadiegoN <ngadiego@gmail.com>
2026-05-29 13:08:10 -0300

    feat: implement simulation form data storage and navigation logic

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,15 +1,29 @@
 import { FormStep } from "./FormStep";
 import { StepProgress } from "./Progress";
 import { simulationFormSteps } from "../../../data/simulation";
+import type { SimulationFormData } from "../../../data/simulation";
 import { useState } from "react";
+import { useSimulationStorage } from "../../../hooks/useSimulationStorage";
+import { useNavigate } from "react-router-dom";
 
 export function SimulationForm() {
+  const { saveFormData } = useSimulationStorage();
+  const navigate = useNavigate();
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
+  const [formData, setFormData] = useState<SimulationFormData>(
+    {} as SimulationFormData,
+  );
   const totalSteps = simulationFormSteps.length;
   const currentStep = simulationFormSteps[currentStepIndex];
 
-  const handleNextStep = () => {
+  const handleNextStep = (value: string) => {
+    const updateFormData = { ...formData, [currentStep.id]: value };
+    setFormData(updateFormData);
+
     if (currentStepIndex + 1 > totalSteps - 1) {
+      saveFormData(updateFormData);
+
+      void navigate("/resultado");
       return;
     }
 
